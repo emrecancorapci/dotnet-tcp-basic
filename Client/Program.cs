a9efc65ddb8d1670ac466fe739af06bf74b213df
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-01-14 13:54:51 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+await StartClient();
+
+async Task StartClient()
+{
+    byte[] data = new byte[1024];
+
+    try
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        const int port = 60_000;
+        IPEndPoint remoteEndPoint = new (ipAddress, port);
+
+        Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine("Socket created with TCP Protocol.");
+
+        try
+        {
+            await sender.ConnectAsync(remoteEndPoint);
+            Console.WriteLine($"Socket connected to [{ipAddress}:{port}]");
+
+            string? messageString;
+            Console.WriteLine("Enter the message.");
+            messageString = Console.ReadLine();
+
+            byte[] messageData = Encoding.UTF8.GetBytes(messageString + "<EOF>");
+
+            int byteSent = await sender.SendAsync(messageData);
+            Console.WriteLine("Message sent.");
+            int byteReceived = await sender.ReceiveAsync(data);
+            Console.WriteLine("Data received.");
+            string receivedMessage = Encoding.UTF8.GetString(messageData, 0, byteReceived);
+
+            Console.WriteLine($"\nEchoed data: {receivedMessage[..^5]}");
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine($"Argument Null Exception : {ex.Message}");
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Socket Exception : {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected Exception : {ex.Message}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
---
+++ Listener/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+await StartServer();
+Console.Read();
+
+async Task StartServer()
+{
+    IPHostEntry host = await Dns.GetHostEntryAsync("localhost");
+    IPAddress ipAddress = host.AddressList[0];
+    const int port = 60_000;
+    IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+    try
+    {
+        Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine("Socket created with TCP Protocol.");
+
+        listener.Bind(localEndPoint);
+        Console.WriteLine($"Endpoint bind at [{ipAddress}:{port}] .");
+
+        listener.Listen(10);
+        Console.WriteLine("Socket started to listen.");
+
+        Socket handler = listener.Accept();
+        Console.WriteLine("Connection accepted. Waiting for message...");
+
+        string receivedMessage = "";
+
+        while (true)
+        {
+            var receivedData = new byte[1024];
+            int receivedBytes = await handler.ReceiveAsync(receivedData);
+            receivedMessage += Encoding.UTF8.GetString(receivedData, 0, receivedBytes);
+
+            if (receivedMessage.IndexOf("<EOF>", StringComparison.Ordinal) > -1) break;
+        }
+
+        Console.WriteLine($"\nMessage received: {receivedMessage[..^5]}");
+
+        byte[] message = Encoding.UTF8.GetBytes(receivedMessage);
+        await handler.SendAsync(message);
+        Console.WriteLine("Received message sent back.");
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
